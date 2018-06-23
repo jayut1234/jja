20211ee5fc0d49ad4ec1959b9064635f91ceba2c
jayut1234 <40523544+jayut1234@users.noreply.github.com>
2018-06-23 23:04:52 +0700

    Add files via upload

---
+++ headphp.php
@@ -0,0 +1,137 @@
+
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js"></script>
+<link href="style.css" type="text/css" rel="stylesheet" >
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="height: 200px;background-color: #6666db;" > 
+
+  <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="padding-top: 10px;padding-right: 0px;">
+        <div class="DCHD" style="float: right; font-size: 30px;">
+          DCHD
+        </div>
+  </div>
+
+  <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" style="padding-left: 8px;padding-top: 15px;">
+
+        <div class="community" style="font-size: 12px;width: 193px;">
+          กองพัฒนาศักยภาพชุมชนและทุนมนุษย์ 
+        </div>
+
+        <div class="commu2" style="font-size: 12px;border-top: 1px solid #000;width: 324px;">
+          DIVISION OF COMMUNITY AND HUMAN DEVELOPMENT
+        </div>
+
+  </div>
+
+
+
+  <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+      <div class="face">
+        face
+
+      </div>
+  </div>
+
+
+</div>
+
+
+</head>
+
+<script>
+  $(document).ready(function() {
+      $('.bxslider').bxSlider({
+      infiniteLoop: false,
+        auto: true,
+        speed: 1000,
+    hideControlOnEnd: true,controls:true
+    });
+     });
+</script>
+
+<div class="bxslider">
+  <div><img src="img/0001.png" title="1"></div>
+  <div><img src="img/0001.png." title="2"></div>
+  <div><img src="img/0001.png" title="3"></div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div class="book">
+      <div><img src="img/0001.png"></div>
+        <div class="textbook">เสริมสร้างกระบวนการ</div>
+    </div>
+  </div>
+
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div class="area"></div>
+      <div><img src="img/0001.png"></div>
+        <div class="textbook">เสริมสร้างกระบวนการ</div>
+  </div>
+
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div class="data"></div>
+      <div><img src="img/0001.png"></div>
+        <div class="textbook">เสริมสร้างกระบวนการ</div>
+  </div>
+
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+    <div class="reletion"></div>
+      <div><img src="img/0001.png"></div>
+        <div class="textbook">เสริมสร้างกระบวนการ</div>
+  </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-left: 25px;padding-right: 25px;margin-top: 30px;">
+<!-- ข่าวกิจกรรม !-->
+    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+      <div style="width: 400px; height: 800px; overflow-y: scroll; scrollbar-arrow-color:blue; scrollbar-face-color: #e7e7e7; scrollbar-3dlight-color: #a0a0a0; scrollbar-darkshadow-color:#888888">  
+        <div class="megaphone" style="display: inline-block;">
+          <img src="img/0001.png">
+        </div>
+          <div class="msg" style="display: inline-block;">
+            โทรโข่ง
+          </div>
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-top: 20px;border-bottom: 1px solid #00BCD4;padding-bottom: 5px;">
+           <a href="img/Untitled.png" data-fancybox="group">
+                  <div class= "gallery_img" style="width: 40%;display: inline-block;">
+                    <img src="img/Untitled.png" style="width: 100%;">
+                     <span class="ico ico-mglass"></span>
+                  </div>
+            </a> 
+            <div class="main" style="display: inline-block;">
+              หัวข้อ<br>
+              <div class="main2" style="display: inline-block;">
+            ..................
+            </div>      
+            </div>  
+                  
+         </div>
+        </div>
+    </div>
+<!-- ///////////////////////////////////////////////////// !-->
+
+
+
+<!-- ข่าววิชาการ !-->
+    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+      
+    </div>
+<!-- ///////////////////////////////////////////////////// !-->
+
+
+
+<!-- login เจ้าหน้าที่ !-->
+    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+      
+    </div>
+<!-- ///////////////////////////////////////////////////// !-->
+
+</div>
